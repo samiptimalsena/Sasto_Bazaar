154b80fe8e942fe2249c890e3abe395795bfa80a
unknown <samip425@gmail.com>
2020-05-10 15:55:36 +0545

    made card description layout

---
+++ client/lib/screen/cardDescription.dart
@@ -0,0 +1,298 @@
+import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' show Vector3;
+import 'package:google_fonts/google_fonts.dart';
+
+class CardDesciption extends StatefulWidget {
+  @override
+  _CardDescriptionState createState() => _CardDescriptionState();
+}
+
+class _CardDescriptionState extends State<CardDesciption> {
+  Offset offset;
+  double _scale = 1.0;
+  int count=1;
+
+  Color mButtonFillColor = Colors.white,
+      lButtonFillColor = Colors.black,
+      xlButtonFillColor = Colors.white,
+      xxlButtonFillColor = Colors.white,
+      sButtonFillColor = Colors.white;
+  Color sButtonBorderColor = Colors.grey[500],
+      mButtonBorderColor = Colors.grey[500],
+      lButtonBorderColor = Colors.black,
+      xlButtonBorderColor = Colors.grey[500],
+      xxlButtonBorderColor = Colors.grey[500];
+  Color sButtonTextColor = Colors.grey[400],
+      mButtonTextColor = Colors.grey[400],
+      lButtonTextColor = Colors.grey[50],
+      xlButtonTextColor = Colors.grey[400],
+      xxlButtonTextColor = Colors.grey[400];
+
+  Widget sizeButton(String text) {
+    return Container(
+      margin: const EdgeInsets.only(left: 10),
+      child: SizedBox(
+        height: 40,
+        width: 40,
+        child: (RaisedButton(
+          elevation: 0,
+          padding: const EdgeInsets.all(0.0),
+          shape: RoundedRectangleBorder(
+              side: BorderSide(
+                  color: text == "S"
+                      ? sButtonBorderColor
+                      : text == "M"
+                          ? mButtonBorderColor
+                          : text == "L"
+                              ? lButtonBorderColor
+                              : text == "XL"
+                                  ? xlButtonBorderColor
+                                  : text == "XXL"
+                                      ? xxlButtonBorderColor
+                                      : null),
+              borderRadius: BorderRadius.circular(2)),
+          color: text == "S"
+              ? sButtonFillColor
+              : text == "M"
+                  ? mButtonFillColor
+                  : text == "L"
+                      ? lButtonFillColor
+                      : text == "XL"
+                          ? xlButtonFillColor
+                          : text == "XXL" ? xxlButtonFillColor : null,
+          onPressed: () {
+            setState(() {
+              if (text == "S") {
+                sButtonFillColor = Colors.black;
+                sButtonBorderColor = Colors.black;
+                sButtonTextColor = Colors.grey[50];
+                mButtonFillColor = lButtonFillColor =
+                    xlButtonFillColor = xxlButtonFillColor = Colors.white;
+                mButtonBorderColor = lButtonBorderColor = xlButtonBorderColor =
+                    xxlButtonBorderColor = Colors.grey[500];
+                mButtonTextColor = lButtonTextColor =
+                    xlButtonTextColor = xxlButtonTextColor = Colors.grey[400];
+              }
+              if (text == "M") {
+                mButtonFillColor = Colors.black;
+                mButtonBorderColor = Colors.black;
+                mButtonTextColor = Colors.grey[50];
+                sButtonFillColor = lButtonFillColor =
+                    xlButtonFillColor = xxlButtonFillColor = Colors.white;
+                sButtonBorderColor = lButtonBorderColor = xlButtonBorderColor =
+                    xxlButtonBorderColor = Colors.grey[500];
+                sButtonTextColor = lButtonTextColor =
+                    xlButtonTextColor = xxlButtonTextColor = Colors.grey[400];
+              }
+              if (text == "L") {
+                lButtonFillColor = Colors.black;
+                lButtonBorderColor = Colors.black;
+                lButtonTextColor = Colors.grey[50];
+                mButtonFillColor = sButtonFillColor =
+                    xlButtonFillColor = xxlButtonFillColor = Colors.white;
+                mButtonBorderColor = sButtonBorderColor = xlButtonBorderColor =
+                    xxlButtonBorderColor = Colors.grey[500];
+                mButtonTextColor = sButtonTextColor =
+                    xlButtonTextColor = xxlButtonTextColor = Colors.grey[400];
+              }
+              if (text == "XL") {
+                xlButtonFillColor = Colors.black;
+                xlButtonBorderColor = Colors.black;
+                xlButtonTextColor = Colors.grey[50];
+                mButtonFillColor = lButtonFillColor =
+                    sButtonFillColor = xxlButtonFillColor = Colors.white;
+                mButtonBorderColor = lButtonBorderColor = sButtonBorderColor =
+                    xxlButtonBorderColor = Colors.grey[500];
+                mButtonTextColor = lButtonTextColor =
+                    sButtonTextColor = xxlButtonTextColor = Colors.grey[400];
+              }
+              if (text == "XXL") {
+                xxlButtonFillColor = Colors.black;
+                xxlButtonBorderColor = Colors.black;
+                xxlButtonTextColor = Colors.grey[50];
+                mButtonFillColor = lButtonFillColor =
+                    xlButtonFillColor = sButtonFillColor = Colors.white;
+                mButtonBorderColor = lButtonBorderColor =
+                    xlButtonBorderColor = sButtonBorderColor = Colors.grey[500];
+                mButtonTextColor = lButtonTextColor =
+                    xlButtonTextColor = sButtonTextColor = Colors.grey[400];
+              }
+            });
+          },
+          textColor: text == "S"
+              ? sButtonTextColor
+              : text == "M"
+                  ? mButtonTextColor
+                  : text == "L"
+                      ? lButtonTextColor
+                      : text == "XL"
+                          ? xlButtonTextColor
+                          : text == "XXL" ? xxlButtonTextColor : null,
+          child: Text(text),
+        )),
+      ),
+    );
+  }
+
+  Widget quantityButton(var icon){
+    return Container(
+      margin: const EdgeInsets.only(left:10),
+      color: icon==Icons.remove?Colors.grey[100]:Colors.grey[200],
+      child:SizedBox(
+      height: 40,width: 40,
+    child: IconButton(
+      onPressed: (){
+        if(count>=1 && icon==Icons.add ){
+          setState(() {
+            count++;
+          });
+        }else if(count>1 && icon==Icons.remove){
+          setState(() {
+            count--;
+          });
+        }
+      },
+      icon: Icon(icon,color:icon==Icons.remove?Colors.grey[350]:Colors.grey[500],),
+    )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var ht = MediaQuery.of(context).size.height;
+    return (Scaffold(
+        appBar: AppBar(
+          iconTheme: new IconThemeData(color: Colors.grey[400]),
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                size: 25,
+              ),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: ListView(
+          children: <Widget>[
+            GestureDetector(
+              onScaleStart: (ScaleStartDetails details) {
+                print(details.localFocalPoint);
+
+                setState(() {
+                  offset = details.localFocalPoint;
+                });
+              },
+              onScaleUpdate: (ScaleUpdateDetails details) {
+                setState(() {
+                  _scale = details.scale;
+                });
+              },
+              onScaleEnd: (ScaleEndDetails details) {
+                setState(() {
+                  _scale = 1.0;
+                });
+              },
+              child: Container(
+                  height: ht / 2.3,
+                  color: Colors.white,
+                  child: Transform(
+                      transform: Matrix4.diagonal3(Vector3(
+                          _scale > 1.0 ? _scale : 1.0,
+                          _scale > 1.0 ? _scale : 1.0,
+                          _scale > 1.0 ? _scale : 1.0)),
+                      origin: offset,
+                      child: Image.asset(
+                        "assets/images/sample.jpg",
+                        fit: BoxFit.fill,
+                      ))),
+            ),
+            Container(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                      margin: const EdgeInsets.only(top: 15),
+                      child: Text("France Authentic",
+                          style: GoogleFonts.roboto(
+                              textStyle: TextStyle(
+                                  fontSize: 28, fontWeight: FontWeight.bold)))),
+                  Container(
+                    margin: const EdgeInsets.only(top: 5),
+                    child: Text("NIKE",
+                        style: TextStyle(
+                            color: Colors.grey[400],
+                            fontWeight: FontWeight.w500,
+                            fontSize: 15)),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 15),
+                    child: Text(
+                      "\$30",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 10),
+                    child: Divider(
+                      indent: 10,
+                      endIndent: 10,
+                      color: Colors.grey[200],
+                      thickness: 1.3,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top:20),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                      margin: const EdgeInsets.only(right: 10, left: 10),
+                      child: Text("Size",style: GoogleFonts.roboto(textStyle:TextStyle(fontSize: 16,fontWeight:FontWeight.bold)),)),
+                  sizeButton("S"),
+                  sizeButton("M"),
+                  sizeButton("L"),
+                  sizeButton("XL"),
+                  sizeButton("XXL")
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top:25),
+              child: Row(children: <Widget>[
+                Container(
+                      margin: const EdgeInsets.only(right: 15, left: 10),
+                      child: Text("Qty",style: GoogleFonts.roboto(textStyle:TextStyle(fontSize: 16,fontWeight:FontWeight.bold)),)),
+                quantityButton(Icons.remove),
+                Container(
+                  margin: const EdgeInsets.only(left:20,right:10),
+                  child: Text("$count",style: GoogleFonts.roboto(textStyle:TextStyle(fontSize: 20,fontWeight:FontWeight.w500)))),
+                quantityButton(Icons.add)
+              ],),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top:10),
+              alignment: Alignment.center,
+              child: RaisedButton.icon(
+                elevation: 0.8,
+                shape:RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16)
+                ) ,
+                onPressed: (){
+
+                },
+                icon: Icon(Icons.add_shopping_cart),
+                label: Text("Add to Cart"),
+                textColor: Colors.white,
+                color: Colors.orange[800],
+              ),
+            )
+          ],
+        )));
+  }
+}
--- client/lib/screen/home.dart
+++ client/lib/screen/home.dart
@@ -3,42 +3,56 @@ import '../auth/login.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:google_fonts/google_fonts.dart';
+import './cardDescription.dart';
 
-class Home extends StatefulWidget{
+ 
+
+class Home extends StatefulWidget {
   @override
-  _HomeState createState()=>_HomeState();
+  _HomeState createState() => _HomeState();
 }
 
-
-class _HomeState extends State<Home>{
+class _HomeState extends State<Home> {
   SharedPreferences sharedPreferences;
-  String userName="achha";
-  final facebookLogin=FacebookLogin();
+  String userName = "achha";
+  final facebookLogin = FacebookLogin();
   GoogleSignIn _googleSignIn = GoogleSignIn(
-  scopes: <String>[
-    'email'
-  ],
-);
+    scopes: <String>['email'],
+  );
 
-  
-  getUserName()async{
-    sharedPreferences=await SharedPreferences.getInstance();
+  getUserName() async {
+    sharedPreferences = await SharedPreferences.getInstance();
     setState(() {
-      userName=sharedPreferences.getString("userName");
+      userName = sharedPreferences.getString("userName");
     });
   }
-  
-  void initState(){
+
+  void initState() {
     super.initState();
     getUserName();
   }
 
   @override
-  Widget build(BuildContext context){
-    return(
-      Scaffold(
-        body: ListView(children: <Widget>[
-          Center(child: Text(userName),),
+  Widget build(BuildContext context) {
+    return (Scaffold(
+      appBar: AppBar(
+        iconTheme: new IconThemeData(color: Colors.grey[400]),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.shopping_cart,
+              size: 25,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[Text("welcome to sasto bazaar"),
           RaisedButton(child: Text("Logout"),
           onPressed: () async{
            sharedPreferences = await SharedPreferences.getInstance();
@@ -51,8 +65,65 @@ class _HomeState extends State<Home>{
            sharedPreferences.clear();
             Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (BuildContext context)=>Login()), (route) => false);
           },)
-        ],)
-      )
-    );
+          ],
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          GestureDetector(
+              onTap: (){
+                Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context)=>CardDesciption()));
+              },
+                      child: Container(
+              height: 220,
+              width: 140,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey[300],
+                        offset: Offset(1.0, 5.0),
+                        blurRadius: 3.0)
+                  ]),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  ClipRRect(
+                    borderRadius: BorderRadius.only(
+                        topLeft: const Radius.circular(10),
+                        topRight: const Radius.circular(10)),
+                    child: SizedBox(
+                        height: 155,
+                        child: Image.asset(
+                          "assets/images/sample.jpg",
+                          fit: BoxFit.cover,
+                        )),
+                  ),
+                  Container(
+                      margin: const EdgeInsets.only(left: 10, top: 7),
+                      child: Text(
+                        "\$30.00",
+                        style: TextStyle(
+                            color: Colors.grey[400],
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16),
+                      )),
+                      Container(
+                      margin: const EdgeInsets.only(left: 10, top: 5),
+                      child: Text(
+                        "Woman T-shirt",
+                        style:GoogleFonts.firaSans(textStyle:TextStyle( color: Colors.grey[400],
+                            fontWeight: FontWeight.w400,
+                            fontSize: 15)),
+                      ))
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
